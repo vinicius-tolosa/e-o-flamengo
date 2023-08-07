96b65c05ed9d7371d7cd4454166ce5ccf174de0b
vinicius-tolosa <141243179+vinicius-tolosa@users.noreply.github.com>
2023-08-07 12:11:53 -0300

    Add files via upload

---
+++ ifelse3.js
@@ -0,0 +1,7 @@
+let senha = 1234
+let senha_inserida = 1234
+
+if(senha_inserida == senha ){
+    console.log("acesso permitido")
+}else{(senha_inserida !== senha )
+console.log("acesso não permitido")}
