1b9ea07ce87ad8083e7ea56f45e49194dc7932ea
vinicius-tolosa <141243179+vinicius-tolosa@users.noreply.github.com>
2023-08-07 11:45:29 -0300

    Add files via upload

---
+++ ifelse1.js
@@ -0,0 +1,10 @@
+//exercicio de if/else
+//questão 1
+let n1 = 2
+let n2 = 4
+console.log("As variaveis são:",n1,",",n2)
+console.log("A variavel maior é:")
+if (n1 > n2){
+console.log(n1)
+}else{ (n1<n2)
+console.log(n2)}
---
+++ ifelse2.js
@@ -0,0 +1,11 @@
+let ano_nasc = 2006
+let min = 16
+let ano = 2024
+let idade = ano-ano_nasc
+console.log("Seu ano de nascimento:",ano_nasc)
+console.log("sua idade é : ",idade)
+if (ano-ano_nasc > min){
+    console.log("apto à votar")
+}else{ (ano - ano_nasc < min)
+    console.log("não está apto à votar")
+}
